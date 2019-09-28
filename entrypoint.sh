9745e29d2c0492993bf2506a8f227d77811c2fa4
Дмитрий Баранов <baranov@semograph.com>
2019-09-28 18:13:04 +0500

    Hide DB passwords to files

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,11 +33,21 @@ EOF
 
 case $DB_TYPE in
 "POSTGRESQL")
-  BEFORE_BACKUP="PGPASSWORD=$POSTGRES_PASSWORD pg_dump ${POSTGRES_DATABASE:-$POSTGRES_USER} -h $DB_SERVER_HOST -U $POSTGRES_USER > /var/backups/dump.sql"
+  if [ -n "$POSTGRES_PASSWORD" ]
+  then
+    printf "%s:%s:%s:%s:%s\n" "${DB_SERVER_HOST}" "${DB_SERVER_PORT:-5432}" "${POSTGRES_DATABASE:-$POSTGRES_USER}" "$POSTGRES_USER" "$POSTGRES_PASSWORD" > $HOME/.pgpass
+    chmod 600 $HOME/.pgpass
+  fi
+  BEFORE_BACKUP="pg_dump ${POSTGRES_DATABASE:-$POSTGRES_USER} -h $DB_SERVER_HOST -U $POSTGRES_USER -w > /var/backups/dump.sql"
   BACKUP_LOCATIONS="/var/backups/dump.sql;$BACKUP_LOCATIONS"
   ;;
 "MYSQL")
-  BEFORE_BACKUP="mysqldump ${MYSQL_DATABASE:-$MYSQL_USER} -h $DB_SERVER_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD > /var/backups/dump.sql"
+  if [ -n "$MYSQL_PASSWORD" ]
+  then
+    printf "[client]\npassword=%s\n" "$MYSQL_PASSWORD" > $HOME/.my.cnf
+    chmod 600 $HOME/.my.cnf
+  fi
+  BEFORE_BACKUP="mysqldump ${MYSQL_DATABASE:-$MYSQL_USER} -h $DB_SERVER_HOST -u $MYSQL_USER > /var/backups/dump.sql"
   BACKUP_LOCATIONS="/var/backups/dump.sql;$BACKUP_LOCATIONS"
   ;;
 *)
@@ -69,5 +79,7 @@ $(for bb in $BEFORE_BACKUP; do printf "    - %s\n" "$bb"; done)
   after_backup:
     - rm -f /var/backups/dump.sql
 EOF
+
+cat /etc/borgmatic.d/backup.yaml
 
 crond -f -L /dev/stdout
