272aa552511b7889e59759567c8b2a5ab83c77c5
Dmitriy Baranov <baranovda89@gmail.com>
2022-06-09 14:00:37 +0500

    Add AFTER_BACKUP variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -92,10 +92,12 @@ consistency:
   prefix: '${BORG_ARCHIVE_NAME:-$HOSTNAME-}'
 
 hooks:
-$([ -n "$BEFORE_BACKUP" ] && echo "  before_backup:")
+echo "  before_backup:"
+    - true
 $(for bb in $BEFORE_BACKUP; do printf "    - %s\n" "$bb"; done)
   after_backup:
-    - rm -f /var/backups/dump.sql
+    - true
+$(for ab in $AFTER_BACKUP; do printf "    - %s\n" "$ab"; done)
 $([ -n "$MAIL_ADDRESS_TO" ] && printf "    - '(printf \"From: %s\\\r\\\nSubject: %s\\\r\\\n\\\r\\\n\"; tail -n 200 /var/backups/borg.log) | ssmtp %s'" "$MAIL_FROM" "$SUBJECT_SUCCESS" "$MAIL_ADDRESS_TO")
 $([ -n "$MAIL_ADDRESS_TO" ] && printf "  on_error:")
 $([ -n "$MAIL_ADDRESS_TO" ] && printf "    - '(printf \"From: %s\\\r\\\nSubject: %s\\\r\\\n\\\r\\\n\"; tail -n 200 /var/backups/borg.log) | ssmtp %s'" "$MAIL_FROM" "$SUBJECT_ERROR" "$MAIL_ADDRESS_TO")
